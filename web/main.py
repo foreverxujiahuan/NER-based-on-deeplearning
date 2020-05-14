d8460a69d1ddd881eb4d2a213cfcbe8c924f6a4d
xujiahuan <jiahuan.xu@videt.cn>
2020-05-14 14:25:21 +0800

    修改web

--- web/main.py
+++ web/main.py
@@ -2,9 +2,9 @@
 @Autor: xujiahuan
 @Date: 2020-05-12 21:41:12
 @LastEditors: xujiahuan
-@LastEditTime: 2020-05-12 23:42:15
+@LastEditTime: 2020-05-14 13:49:16
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -18,6 +18,18 @@ def index():
 @app.route("/xjh")
 def xjh():
     return "你好,我叫许嘉欢"
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("pwd")
+    print(username)
+    print(password)
+    if username == "xjh":
+        return "成功"
+    else:
+        return "失败"
 
 
 if __name__ == '__main__':
